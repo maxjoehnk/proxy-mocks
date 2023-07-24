5e8bc92a4a8d2d0750c9155465e8cdaf85fa4205
Max Jöhnk <maxjoehnk@gmail.com>
2023-07-24 14:30:56 +0200

    fix(vitest): vi is only available as a global variable instead of es import

--- src/vitest.ts
+++ src/vitest.ts
@@ -1,7 +1,11 @@
 import * as vitest from "vitest";
-import { createMockImplementationWithStubFunction, MockableObject, MockPrototype } from './mock';
-import * as base from './mock';
+import { createMockImplementationWithStubFunction, MockableObject, MockPrototype } from "./mock";
+import * as base from "./mock";
 
 export type IMock<TObject extends MockableObject> = base.IMock<TObject, vitest.Mock>;
 
-export const Mock: MockPrototype<vitest.Mock> = createMockImplementationWithStubFunction(vitest.vi.fn);
+// TODO: each test framework specific way should be tested in it's own environment
+// This fixes the current issue where we can't import the vitest globals as they conflict with jest globals
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export const Mock: MockPrototype<vitest.Mock> = createMockImplementationWithStubFunction(vi.fn);
